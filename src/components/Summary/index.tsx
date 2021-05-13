51c35c3b7af3169c4277d6bd4f8cd8575cf59a50
Alex Borelli <alex.sander.borelli@outlook.com.br>
2021-05-13 10:46:19 -0300

    Components Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,39 @@
+import IncomeImg from '../../assets/income.svg'
+import OutcomeImg from '../../assets/outcome.svg'
+import Totalimg from '../../assets/total.svg'
+
+import { Container } from "./styles";
+
+export function Summary(){
+  return(
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={IncomeImg} alt="Entradas"/> 
+        </header>
+        <strong>
+          R$ 1000,00
+        </strong>
+      </div>  
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={OutcomeImg} alt="Saídas"/> 
+        </header>
+        <strong>
+          - R$ 500,00
+        </strong>
+      </div>  
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={Totalimg} alt="Total"/> 
+        </header>
+        <strong>
+          R$ 500,00
+        </strong>
+      </div>  
+    </Container>
+  )
+}

9d207d3fd8b117e088622b40e53dcfdc2ece2bf2
Alex Borelli <alex.sander.borelli@outlook.com.br>
2021-05-14 15:12:58 -0300

    Buttons styles, form input states

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return data;
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
